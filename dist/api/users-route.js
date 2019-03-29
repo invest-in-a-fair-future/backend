7e738252bab85d77dc4d6f845eee13fec7d1f697
M. Zafer Yahsi <mzaferyahsi@gmail.com>
2019-03-29 17:41:08 +0100

    Fallback to mocking

--- dist/api/users-route.js
+++ dist/api/users-route.js
@@ -43,73 +43,86 @@ function () {
   }, {
     key: "configure",
     value: function configure(router) {
-      var _this = this;
-
+      // router.post('/users', (req, resp) => {
+      //   const user = req.body;
+      //
+      //   if(!user) {
+      //     return resp.status(400).json({
+      //       'code': 'INVCRUSR01',
+      //       'message': 'No user information sent.'
+      //     });
+      //   }
+      //
+      //   if(!user.email || !user.name || !user.type || !user.password || !user.country) {
+      //     return resp.status(400).json({
+      //       'code': 'INVCRUSR02',
+      //       'message': 'Invalid or missing information.'
+      //     });
+      //   } else if(user.type !== 'company' && !user.type !== 'partner' && !user.type !== 'comminityRepresentative') {
+      //     return resp.status(400).json({
+      //       'code': 'INVCRUSR02',
+      //       'message': 'Invalid or missing information.'
+      //     });
+      //   }
+      //
+      //   user.password = bcrypt.hashSync(user.password, config.users.salt);
+      //
+      //   DB.getCollection('users')
+      //     .then(users => {
+      //       return users.findOne({email:user.email})
+      //         .then(dbUser => {
+      //           if(!dbUser) {
+      //             return users.insertOne(user);
+      //           } else {
+      //             const e = new Error('User already exists');
+      //             e.passMessage = true;
+      //             throw e;
+      //           }
+      //         });
+      //     })
+      //     .then(result => {
+      //       if(result.result.ok < 1) {
+      //         throw new Error('Unable to add user');
+      //       }
+      //       const createdUser = result.ops[0];
+      //       delete createdUser.password;
+      //       createdUser.id = createdUser._id;
+      //       delete createdUser._id;
+      //       const credentials = Auth.generateToken(createdUser);
+      //       return resp.status(201).json({
+      //         user: createdUser,
+      //         credentials: credentials
+      //       });
+      //     })
+      //     .catch(e => {
+      //       this.getLogger().error(e);
+      //       if(e.passMessage) {
+      //         return resp.status(500).json({
+      //           code: 'INVCRUSR501',
+      //           message: e.message
+      //         });
+      //
+      //       } else {
+      //         return resp.status(500).json({
+      //           code: 'INVCRUSR500',
+      //           message: 'Unable to create user.'
+      //         });
+      //       }
+      //     });
+      // });
       router.post('/users', function (req, resp) {
-        var user = req.body;
-
-        if (!user) {
-          return resp.status(400).json({
-            'code': 'INVCRUSR01',
-            'message': 'No user information sent.'
-          });
-        }
-
-        if (!user.email || !user.name || !user.type || !user.password || !user.country) {
-          return resp.status(400).json({
-            'code': 'INVCRUSR02',
-            'message': 'Invalid or missing information.'
-          });
-        } else if (user.type !== 'company' && !user.type !== 'partner' && !user.type !== 'comminityRepresentative') {
-          return resp.status(400).json({
-            'code': 'INVCRUSR02',
-            'message': 'Invalid or missing information.'
-          });
-        }
-
-        user.password = _bcrypt.default.hashSync(user.password, _config.default.users.salt);
-
-        _DB.DB.getCollection('users').then(function (users) {
-          return users.findOne({
-            email: user.email
-          }).then(function (dbUser) {
-            if (!dbUser) {
-              return users.insertOne(user);
-            } else {
-              var e = new Error('User already exists');
-              e.passMessage = true;
-              throw e;
-            }
-          });
-        }).then(function (result) {
-          if (result.result.ok < 1) {
-            throw new Error('Unable to add user');
-          }
-
-          var createdUser = result.ops[0];
-          delete createdUser.password;
-          createdUser.id = createdUser._id;
-          delete createdUser._id;
-
-          var credentials = _auth.Auth.generateToken(createdUser);
-
-          return resp.status(201).json({
-            user: createdUser,
-            credentials: credentials
-          });
-        }).catch(function (e) {
-          _this.getLogger().error(e);
-
-          if (e.passMessage) {
-            return resp.status(500).json({
-              code: 'INVCRUSR501',
-              message: e.message
-            });
-          } else {
-            return resp.status(500).json({
-              code: 'INVCRUSR500',
-              message: 'Unable to create user.'
-            });
+        resp.status(201).json({
+          "user": {
+            "email": "john4@investinafairfuture.org",
+            "name": "Invest in a Fair Future",
+            "type": "company",
+            "phoneNumber": "+31600000000",
+            "country": "NL",
+            "id": "5c9e4a1382449723828186f3"
+          },
+          "credentials": {
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG40QGludmVzdGluYWZhaXJmdXR1cmUub3JnIiwibmFtZSI6IkludmVzdCBpbiBhIEZhaXIgRnV0dXJlIiwidHlwZSI6ImNvbXBhbnkiLCJwaG9uZU51bWJlciI6IiszMTYwMDAwMDAwMCIsImNvdW50cnkiOiJOTCIsImlkIjoiNWM5ZTRhMTM4MjQ0OTcyMzgyODE4NmYzIiwiaWF0IjoxNTUzODc3NTIzLCJleHAiOjE1NTY0Njk1MjN9.ORcfbjvB8iOFTF_PfJ0GC4PvwUqhn0bmmZ948z-pGjc",
+            "validUntil": "2019-04-28T15:38:43.480Z"
           }
         });
       });
